72b5bdf18080ad18a0410a34e73d0a1281f54675
Davido <d@davidodonovan.com>
2021-07-05 07:52:15 +1000

    install 'commander' npm package

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
 const madge = require('madge');
+const { program } = require('commander');
+const fs = require('fs');
 
-//madge('../three-js-2021/src').then((res)=>{
-//  console.log(res);
-//});
+console.log("attempting to map dependencies - if you're in SM this may take a while..");
 
+madge('../staff-app/src/Finance/_GeneralLedger/GeneralJournalEntry/Components/SelectGeneralJournalPosting/SelectGeneralJournalPosting.js')
+  .then((res) => res.image('./images/SelectGeneralJournalPosting.pdf'))
+  .then((writtenImagePath) => {
+    console.log('Image written to ' + writtenImagePath);
+   });
 
-madge('../staff-app/src/Community/Common/BaseMaintain/BaseDetailView.js')
-  .then((res)=>{
-    console.log(res.obj());
-})
-  
-	//.then((res) => res.image('./images/BaseDetailView.pdf'))
-	//.then((writtenImagePath) => {
-	//  console.log('Image written to ' + writtenImagePath);
-	// });
